033d568061edb5ac83df6388244755327b2489d2
Cedrick Monare <162999186+CedrickMonare@users.noreply.github.com>
2024-07-12 13:40:12 +0200

    Fixed the way the velocity function was called and started with my try function

--- MATMON606_FTO2403_GroupB_MatlhogonoloMonare_DJS01/index.js
+++ MATMON606_FTO2403_GroupB_MatlhogonoloMonare_DJS01/index.js
@@ -5,13 +5,22 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const calculateNewVelocity = ({ givenVelocity, acceleration, time }) => {
+  const givenVelocityInMps = (givenVelocity * 1000) / 3600; // Function converts velocity from km/h to m/s
+  const newVelocityInMps = givenVelocityInMps + acceleration * time; // Calculates new velocity in m/s
+  return (newVelocityInMps * 3600) / 1000; // Function converts new velocity back to km/h
+};
+
+try {
+  // Given parameters 
+  const givenParameters = {
+    initialVelocity: 10000, // / velocity (km/h)
+    acceleration: 3, // acceleration m/s^2
+    time: 3600, // seconds (1 hour)
+    initialDistance: 0, // distance (km)
+    Fuel: 5000, // remaining fuel (kg)
+    fuelBurnRate: 0.5, // fuel burn rate (kg/s)
+  };
 
 
 const d2 = d + (vel*time) //calcultes new distance
